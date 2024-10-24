53e6d67fb47ec562df7baf387e70ab4aeec36ecb
Gopal Agarwal <agarwalgopal247@gmail.com>
2024-10-24 18:52:48 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("starts the server");
 });
 
